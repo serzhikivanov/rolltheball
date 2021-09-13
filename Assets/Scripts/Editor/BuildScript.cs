aa82a8b02ee85d690e0ab68e07e39eb81e44e5e7
sivanov <sivanov@hill30.com>
2021-09-13 11:38:02 +0300

    Initial commit Project has one scene with rolling ball Unity SBP package installed Build script for one scene for windows x64 created Build batch file added Basic .gitignore

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+public class BuildScript
+{
+    public static void PerformWindowsx64Build()
+    {
+        string[] targetScenes = { "Assets/Scenes/SampleScene.unity" };
+        BuildPipeline.BuildPlayer(
+            targetScenes,
+            "./builds/RollTheBall_x64.exe",
+            BuildTarget.StandaloneWindows64,
+            BuildOptions.None);
+    }
+}
---
+++ BuildScripts/deploy.bat
@@ -0,0 +1,5 @@
+echo Path is "c:\Program Files\Unity\Hub\Editor\2019.4.28f1\Editor\Unity.exe" 
+
+"c:\Program Files\Unity\Hub\Editor\2019.4.28f1\Editor\Unity.exe" -quit -batchmode -projectPath "..\" -executeMethod BuildScript.PerformWindowsx64Build
+
+echo Build finished
