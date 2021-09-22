ee301297e9db5892c61149a2f8b0c80e7e91c3c9
Sergey Ivanov <sivanov@hill30.com>
2021-09-22 19:07:52 +0300

    Commented out player tests for now, added build commands and copying to Appcenter

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -2,13 +2,41 @@
 
 public class BuildScript
 {
+    private static string[] _targetScenes = { "Assets/Scenes/SampleScene.unity" };
+
     public static void PerformWindowsx64Build()
     {
-        string[] targetScenes = { "Assets/Scenes/SampleScene.unity" };
         BuildPipeline.BuildPlayer(
-            targetScenes,
+            _targetScenes,
             "./builds/Winx64/RollTheBall_x64.exe",
             BuildTarget.StandaloneWindows64,
+            BuildOptions.None);
+    }
+
+    public static void PerformAndroidBuild()
+    {
+        BuildPipeline.BuildPlayer(
+            _targetScenes,
+            "./builds/Winx64/RollTheBall_Android.apk",
+            BuildTarget.Android,
+            BuildOptions.None);
+    }
+
+    public static void PerformIOsBuild()
+    {
+        BuildPipeline.BuildPlayer(
+            _targetScenes,
+            "./builds/Winx64/RollTheBall_Android.ipa",
+            BuildTarget.iOS,
+            BuildOptions.None);
+    }
+
+    public static void PerformWebGLBuild()
+    {
+        BuildPipeline.BuildPlayer(
+            _targetScenes,
+            "./builds/WebGL/",
+            BuildTarget.WebGL,
             BuildOptions.None);
     }
 }
